c55563f1b38cfec8dabfbe843171d9ea9da0bc02
unknown <robinkaris7@gmail.com>
2023-04-15 22:48:22 +0300

    made changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -121,7 +121,7 @@
        --}}
     <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-            <span class="font-semibold text-xl tracking-tight">Inventory</span>
+            <a href="/" class="font-semibold text-xl tracking-tight hover:text-white">Inventory</a>
           </div>
           <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
