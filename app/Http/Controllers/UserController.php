21eacda53b5c4743d707685f72eaa07ef3d5fee3
unknown <robinkaris7@gmail.com>
2023-04-17 15:34:50 +0300

    added logout  logic to controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,7 +33,19 @@ class UserController extends Controller
         return redirect('/')->with('message','User created and logged in');
     }
 
+     //Logout User
+     public function logout(Request $request){
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message','you have been Logged out');
+
+     }
+
     
+
 }
 
 
