c4224aa6caf68996441a6de81d6fc4aee70029f4
unknown <robinkaris7@gmail.com>
2023-04-17 15:30:08 +0300

    added register logic to controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,4 +12,28 @@ class UserController extends Controller
     public function create(){
         return view('users.register');
     }
+
+
+
+    //create New user
+    public function store(Request $request){
+        $formFields = $request->validate([
+            'name'=>['required','min:3'],
+            'password'=>'required|confirmed|min:6'
+        ]);
+        //Hash Password
+        $formFields['password'] = bcrypt($formFields['password']);
+        
+        //create User
+        $user = User::create($formFields);
+
+
+        //Login
+        auth()->login($user);
+        return redirect('/')->with('message','User created and logged in');
+    }
+
+    
 }
+
+
